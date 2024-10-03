c4f51a563c470bd6df41d1495a89c700b1c56d76
nexer1234 <77268699+Pro666Pro@users.noreply.github.com>
2024-10-03 15:44:22 +0900

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,6 @@
+local GameName = "water glove script - by nexer"
 
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local Window = OrionLib:MakeWindow({IntroText = "water glove script", IntroIcon = "rbxassetid://15315284749",Name = GameName, HidePremium = false, SaveConfig = true, ConfigFolder = "Tutorial"})
+
+OrionLib:MakeNotification({Name = "Warning",Content = "Use at your own risk.",Image = "rbxassetid://7733658504",Time = 5})
