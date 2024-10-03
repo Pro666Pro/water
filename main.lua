5a0ede00b68dcaa073aa69c59cd4b49169d0f54d
nexer1234 <77268699+Pro666Pro@users.noreply.github.com>
2024-10-03 20:11:16 +0900

    Update main.lua

--- main.lua
+++ main.lua
@@ -7,7 +7,6 @@ OrionLib:MakeNotification({Name = "Warning",Content = "Use at your own risk.",Im
 
 _G.GetPotion = {
 	["Grug"] = {"Mushroom"},
-	["idIot"] = {"Cake Mix"},
 	["Nightmare"] = {"Dark Root","Dark Root","Dark Root"},
 	["Confusion"] = {"Red Crystal","Blue Crystal","Glowing Mushroom"},
 	["Power"] = {"Dire Flower","Red Crystal","Wild Vine"},
@@ -26,3 +25,172 @@ _G.GetPotion = {
 	["Corrupted Vine"] = {"Wild Vine","Wild Vine","Wild Vine","Blood Rose","Dark Root","Elder Wood","Jade Stone"},
 	["Field"] = {"Hazel Lily","Plane Flower","Plane Flower"}
 }
+
+_G.MakePotion1 = "Grug"
+_G.MakePotion2 = "Nightmare"
+_G.MakePotion3 = "Confusion"
+_G.MakePotion4 = "Power"
+_G.MakePotion5 = "Paralyzing"
+_G.MakePotion6 = "Haste"
+_G.MakePotion7 = "Invisibility"
+_G.MakePotion8 = "Explosion"
+_G.MakePotion9 = "Invincible"
+_G.MakePotion10 = "Toxic"
+_G.MakePotion11 = "Freeze"
+_G.MakePotion12 = "Feather"
+_G.MakePotion13 = "Speed"
+_G.MakePotion14 = "Lethal"
+_G.MakePotion15 = "Slow"
+_G.MakePotion16 = "Antitoxin"
+_G.MakePotion17 = "Corrupted Vine"
+_G.MakePotion18 = "Field"
+
+_G.GivePotion = 1
+
+local Badge = Window:MakeTab({
+	Name = "Water Glove",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+Badge:AddButton({
+	Name = "Water Glove",
+	Callback = function()
+if game.Players.LocalPlayer.leaderstats.Glove.Value == "Alchemist" then
+if not game.Workspace:FindFirstChild(game.Players.LocalPlayer.Name.."'s Cauldron") then
+game:GetService("ReplicatedStorage").GeneralAbility:FireServer()
+end
+for b = 1, _G.GivePotion do
+if not game.Workspace:FindFirstChild(game.Players.LocalPlayer.Name.."'s Cauldron") then
+game:GetService("ReplicatedStorage").GeneralAbility:FireServer()
+end
+for i = 1, #_G.GetPotion[_G.MakePotion1] do
+game.ReplicatedStorage:WaitForChild("AlchemistEvent"):FireServer(unpack({"AddItem", _G.GetPotion[_G.MakePotion1][i]}))
+game.ReplicatedStorage:WaitForChild("AlchemistEvent"):FireServer(unpack({"MixItem", _G.GetPotion[_G.MakePotion1][i]}))
+end
+game.ReplicatedStorage:WaitForChild("AlchemistEvent"):FireServer(unpack({"BrewPotion"}))
+task.wait()
+end
+for i = 1, #_G.GetPotion[_G.MakePotion2] do
+game.ReplicatedStorage:WaitForChild("AlchemistEvent"):FireServer(unpack({"AddItem", _G.GetPotion[_G.MakePotion2][i]}))
+game.ReplicatedStorage:WaitForChild("AlchemistEvent"):FireServer(unpack({"MixItem", _G.GetPotion[_G.MakePotion2][i]}))
+end
+game.ReplicatedStorage:WaitForChild("AlchemistEvent"):FireServer(unpack({"BrewPotion"}))
+task.wait()
+end
+for i = 1, #_G.GetPotion[_G.MakePotion3] do
+game.ReplicatedStorage:WaitForChild("AlchemistEvent"):FireServer(unpack({"AddItem", _G.GetPotion[_G.MakePotion3][i]}))
+game.ReplicatedStorage:WaitForChild("AlchemistEvent"):FireServer(unpack({"MixItem", _G.GetPotion[_G.MakePotion3][i]}))
+end
+game.ReplicatedStorage:WaitForChild("AlchemistEvent"):FireServer(unpack({"BrewPotion"}))
+task.wait()
+end
+for i = 1, #_G.GetPotion[_G.MakePotion4] do
+game.ReplicatedStorage:WaitForChild("AlchemistEvent"):FireServer(unpack({"AddItem", _G.GetPotion[_G.MakePotion4][i]}))
+game.ReplicatedStorage:WaitForChild("AlchemistEvent"):FireServer(unpack({"MixItem", _G.GetPotion[_G.MakePotion4][i]}))
+end
+game.ReplicatedStorage:WaitForChild("AlchemistEvent"):FireServer(unpack({"BrewPotion"}))
+task.wait()
+end
+for i = 1, #_G.GetPotion[_G.MakePotion5] do
+game.ReplicatedStorage:WaitForChild("AlchemistEvent"):FireServer(unpack({"AddItem", _G.GetPotion[_G.MakePotion5][i]}))
+game.ReplicatedStorage:WaitForChild("AlchemistEvent"):FireServer(unpack({"MixItem", _G.GetPotion[_G.MakePotion5][i]}))
+end
+game.ReplicatedStorage:WaitForChild("AlchemistEvent"):FireServer(unpack({"BrewPotion"}))
+task.wait()
+end
+for i = 1, #_G.GetPotion[_G.MakePotion6] do
+game.ReplicatedStorage:WaitForChild("AlchemistEvent"):FireServer(unpack({"AddItem", _G.GetPotion[_G.MakePotion6][i]}))
+game.ReplicatedStorage:WaitForChild("AlchemistEvent"):FireServer(unpack({"MixItem", _G.GetPotion[_G.MakePotion6][i]}))
+end
+game.ReplicatedStorage:WaitForChild("AlchemistEvent"):FireServer(unpack({"BrewPotion"}))
+task.wait()
+end
+for i = 1, #_G.GetPotion[_G.MakePotion7] do
+game.ReplicatedStorage:WaitForChild("AlchemistEvent"):FireServer(unpack({"AddItem", _G.GetPotion[_G.MakePotion7][i]}))
+game.ReplicatedStorage:WaitForChild("AlchemistEvent"):FireServer(unpack({"MixItem", _G.GetPotion[_G.MakePotion7][i]}))
+end
+game.ReplicatedStorage:WaitForChild("AlchemistEvent"):FireServer(unpack({"BrewPotion"}))
+task.wait()
+end
+for i = 1, #_G.GetPotion[_G.MakePotion8] do
+game.ReplicatedStorage:WaitForChild("AlchemistEvent"):FireServer(unpack({"AddItem", _G.GetPotion[_G.MakePotion8][i]}))
+game.ReplicatedStorage:WaitForChild("AlchemistEvent"):FireServer(unpack({"MixItem", _G.GetPotion[_G.MakePotion8][i]}))
+end
+game.ReplicatedStorage:WaitForChild("AlchemistEvent"):FireServer(unpack({"BrewPotion"}))
+task.wait()
+end
+for i = 1, #_G.GetPotion[_G.MakePotion9] do
+game.ReplicatedStorage:WaitForChild("AlchemistEvent"):FireServer(unpack({"AddItem", _G.GetPotion[_G.MakePotion9][i]}))
+game.ReplicatedStorage:WaitForChild("AlchemistEvent"):FireServer(unpack({"MixItem", _G.GetPotion[_G.MakePotion9][i]}))
+end
+game.ReplicatedStorage:WaitForChild("AlchemistEvent"):FireServer(unpack({"BrewPotion"}))
+task.wait()
+end
+for i = 1, #_G.GetPotion[_G.MakePotion10] do
+game.ReplicatedStorage:WaitForChild("AlchemistEvent"):FireServer(unpack({"AddItem", _G.GetPotion[_G.MakePotion10][i]}))
+game.ReplicatedStorage:WaitForChild("AlchemistEvent"):FireServer(unpack({"MixItem", _G.GetPotion[_G.MakePotion10][i]}))
+end
+game.ReplicatedStorage:WaitForChild("AlchemistEvent"):FireServer(unpack({"BrewPotion"}))
+task.wait()
+end
+for i = 1, #_G.GetPotion[_G.MakePotion11] do
+game.ReplicatedStorage:WaitForChild("AlchemistEvent"):FireServer(unpack({"AddItem", _G.GetPotion[_G.MakePotion11][i]}))
+game.ReplicatedStorage:WaitForChild("AlchemistEvent"):FireServer(unpack({"MixItem", _G.GetPotion[_G.MakePotion11][i]}))
+end
+game.ReplicatedStorage:WaitForChild("AlchemistEvent"):FireServer(unpack({"BrewPotion"}))
+task.wait()
+end
+for i = 1, #_G.GetPotion[_G.MakePotion12] do
+game.ReplicatedStorage:WaitForChild("AlchemistEvent"):FireServer(unpack({"AddItem", _G.GetPotion[_G.MakePotion12][i]}))
+game.ReplicatedStorage:WaitForChild("AlchemistEvent"):FireServer(unpack({"MixItem", _G.GetPotion[_G.MakePotion12][i]}))
+end
+game.ReplicatedStorage:WaitForChild("AlchemistEvent"):FireServer(unpack({"BrewPotion"}))
+task.wait()
+end
+for i = 1, #_G.GetPotion[_G.MakePotion13] do
+game.ReplicatedStorage:WaitForChild("AlchemistEvent"):FireServer(unpack({"AddItem", _G.GetPotion[_G.MakePotion13][i]}))
+game.ReplicatedStorage:WaitForChild("AlchemistEvent"):FireServer(unpack({"MixItem", _G.GetPotion[_G.MakePotion13][i]}))
+end
+game.ReplicatedStorage:WaitForChild("AlchemistEvent"):FireServer(unpack({"BrewPotion"}))
+task.wait()
+end
+for i = 1, #_G.GetPotion[_G.MakePotion14] do
+game.ReplicatedStorage:WaitForChild("AlchemistEvent"):FireServer(unpack({"AddItem", _G.GetPotion[_G.MakePotion14][i]}))
+game.ReplicatedStorage:WaitForChild("AlchemistEvent"):FireServer(unpack({"MixItem", _G.GetPotion[_G.MakePotion14][i]}))
+end
+game.ReplicatedStorage:WaitForChild("AlchemistEvent"):FireServer(unpack({"BrewPotion"}))
+task.wait()
+end
+for i = 1, #_G.GetPotion[_G.MakePotion15] do
+game.ReplicatedStorage:WaitForChild("AlchemistEvent"):FireServer(unpack({"AddItem", _G.GetPotion[_G.MakePotion15][i]}))
+game.ReplicatedStorage:WaitForChild("AlchemistEvent"):FireServer(unpack({"MixItem", _G.GetPotion[_G.MakePotion15][i]}))
+end
+game.ReplicatedStorage:WaitForChild("AlchemistEvent"):FireServer(unpack({"BrewPotion"}))
+task.wait()
+end
+for i = 1, #_G.GetPotion[_G.MakePotion16] do
+game.ReplicatedStorage:WaitForChild("AlchemistEvent"):FireServer(unpack({"AddItem", _G.GetPotion[_G.MakePotion16][i]}))
+game.ReplicatedStorage:WaitForChild("AlchemistEvent"):FireServer(unpack({"MixItem", _G.GetPotion[_G.MakePotion16][i]}))
+end
+game.ReplicatedStorage:WaitForChild("AlchemistEvent"):FireServer(unpack({"BrewPotion"}))
+task.wait()
+end
+for i = 1, #_G.GetPotion[_G.MakePotion17] do
+game.ReplicatedStorage:WaitForChild("AlchemistEvent"):FireServer(unpack({"AddItem", _G.GetPotion[_G.MakePotion17][i]}))
+game.ReplicatedStorage:WaitForChild("AlchemistEvent"):FireServer(unpack({"MixItem", _G.GetPotion[_G.MakePotion17][i]}))
+end
+game.ReplicatedStorage:WaitForChild("AlchemistEvent"):FireServer(unpack({"BrewPotion"}))
+task.wait()
+end
+for i = 1, #_G.GetPotion[_G.MakePotion18] do
+game.ReplicatedStorage:WaitForChild("AlchemistEvent"):FireServer(unpack({"AddItem", _G.GetPotion[_G.MakePotion18][i]}))
+game.ReplicatedStorage:WaitForChild("AlchemistEvent"):FireServer(unpack({"MixItem", _G.GetPotion[_G.MakePotion18][i]}))
+end
+game.ReplicatedStorage:WaitForChild("AlchemistEvent"):FireServer(unpack({"BrewPotion"}))
+task.wait()
+end
+else
+OrionLib:MakeNotification({Name = "error",Content = "Equip Alchemist >:(",Image = "rbxassetid://7733658504",Time = 5})
+end
+})
